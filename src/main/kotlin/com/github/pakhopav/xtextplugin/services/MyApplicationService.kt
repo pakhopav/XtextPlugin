2cf2daa78c3715e9ca28de91c32ad319b599e0e2
GitHub Action <action@github.com>
2021-08-11 11:22:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/pakhopav/xtextplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pakhopav/xtextplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pakhopav.xtextplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pakhopav.xtextplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/pakhopav/xtextplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pakhopav/xtextplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pakhopav.xtextplugin.services
 
+import com.github.pakhopav.xtextplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
